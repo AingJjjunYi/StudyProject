c731fb15a46e2243d8a8e007e84b638a7c09b359
아잉쥰이 <ilym1l@naver.com>
2023-11-18 15:39:23 +0900

    feat: 프로젝트 생성

---
+++ Source/UEStudyproject/UEStudyproject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UEStudyproject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UEStudyproject, "UEStudyproject" );
---
+++ Source/UEStudyproject/UEStudyprojectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UEStudyprojectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UESTUDYPROJECT_API AUEStudyprojectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
